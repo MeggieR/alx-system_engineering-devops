75a07962a9c8410698b3d9cf5eba2c05ab67a85e
MeggieR <mrotrigs@gmail.com>
2024-05-07 12:49:32 -0700

    m

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,3 +1,3 @@
 #!/usr/bin/env ruby
-#create a Ruby script that accepts one argument and pass it to a regular expression matching method
+#create a Ruby script that accepts one argument and pass it to a regular expression matching met
 puts ARGV[0].scan(/School/).join
